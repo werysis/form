a5e1e05daaf602785751646379e1813d003a99a0
werysis <werysis1@gmail.com>
2016-11-24 18:04:04 +0300

    1

---
+++ script.js
@@ -0,0 +1,17 @@
+﻿function validationCtrl($scope) {
+   var validUsername = "Гаррик Поттер";
+   var validEmail = "hogwards@mail.com";
+   
+   $scope.reset = function(){
+		$scope.username = validUsername;
+		$scope.email = validEmail;
+   }   
+   
+   $scope.checkData = function() {
+		if ($scope.username != validUsername || $scope.email != validEmail) {
+			alert("Увы вы нам не подходите:(");
+		} else {
+			alert("Нормасик!");
+		}
+	}
+}
